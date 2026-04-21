ca3d28278b58acaef00da86b21946ab69c13e7f3
Mathieu Harison <hei.mathieu.95@gmail.com>
2026-04-21 16:11:56 +0300

    Initial commit

---
+++ src/test/java/org/example/tdfinalprog3/TdFinalProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tdfinalprog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdFinalProg3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
